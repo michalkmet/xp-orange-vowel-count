28fd8c2bca0ff06fa79052f2bc12d3f58c2fd70b
Michal Kmet <Michal.Kmet1@ibm.com>
2024-05-20 13:33:36 +0200

    feat: uat1.2 pass

--- src/vowel-count.test.js
+++ src/vowel-count.test.js
@@ -4,4 +4,7 @@ describe('User Story 1: vowels count', () => {
   it("UAT1.1 When I pass 'a' then I should see 1", () => {
     expect(vowelCount('a')).toBe(1);
   });
+  it("UAT1.2 When I pass 'e' then I should see 1", () => {
+    expect(vowelCount('e')).toBe(1);
+  });
 });
